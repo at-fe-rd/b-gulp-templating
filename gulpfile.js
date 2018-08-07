2475d1c16205a87f3ee76f81d70a15aa49051cfb
at-quando <quan.do@asiantech.vn>
2018-08-07 17:34:11 +0700

    eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,49 @@
-var gulp = require('gulp');
-var minifyCSS = require('gulp-csso');
+const gulp = require('gulp');
+const minifyCSS = require('gulp-csso');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const del = require('del');
-var ejs = require('gulp-ejs');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-var sass = require('gulp-sass');
+const ejs = require('gulp-ejs');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
 const runSequence = require('run-sequence');
 
 const plugins = gulpLoadPlugins();
 
-gulp.task('html', function(){
-  return gulp.src('views/pages/*.ejs')
-   .pipe(ejs({}, {}, {ext:'.html'}))
-   .pipe(gulp.dest('dist/'))
-});
+gulp.task('html', () => gulp.src('views/pages/*.ejs')
+  .pipe(ejs({}, {}, { ext: '.html' }))
+  .pipe(gulp.dest('dist/')));
 
-gulp.task('clean', () =>
-  del.sync(['dist/**', '!dist'])
-);
+gulp.task('clean', () => del.sync(['dist/**', '!dist']));
 
-gulp.task('assets', function(){
-  return gulp.src('assets/**/*.*')
-    .pipe(gulp.dest('dist/assets'))
-});
+gulp.task('assets', () => gulp.src('assets/**/*.*')
+  .pipe(gulp.dest('dist/assets')));
 
-gulp.task('css', function(){
-  return gulp.src('stylesheet/scss/styles.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('dist/css'))
-});
+gulp.task('css', () => gulp.src('stylesheet/scss/styles.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(minifyCSS())
+  .pipe(gulp.dest('dist/css')));
 
-gulp.task('js', function(){
-  return gulp.src('js/**/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(concat('app.min.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/js'))
-});
+gulp.task('js', () => gulp.src('js/**/*.js')
+  .pipe(sourcemaps.init())
+  .pipe(concat('app.min.js'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('dist/js')));
 
-gulp.task('watch', function() { 
+gulp.task('watch', () => {
   gulp.watch('stylesheet/scss/**/*.scss', ['css']);
   gulp.watch('js/**/*.js', ['js']);
-  gulp.watch('views/**/*.ejs', ['html']); 
-  gulp.watch('assets/**/*.*', ['assets']); 
+  gulp.watch('views/**/*.ejs', ['html']);
+  gulp.watch('assets/**/*.*', ['assets']);
 });
 
-gulp.task('nodemon',['html', 'css', 'js', 'assets', 'watch'], function() {
+gulp.task('nodemon', ['html', 'css', 'js', 'assets', 'watch'], () => {
   plugins.nodemon({
     script: 'server.js',
     ext: 'js',
     ignore: ['node_modules/**/*.js', 'dist/**/*.js'],
-    tasks: ['html', 'css', 'js', 'assets', 'watch']
-  })
+    tasks: ['html', 'css', 'js', 'assets', 'watch'],
+  });
 });
 
 gulp.task('serve', ['clean'], () => runSequence('nodemon'));
--- server.js
+++ server.js
@@ -1,52 +1,43 @@
-var express = require('express');
-var path= require('path');
-var expressLayouts = require('express-ejs-layouts');
-var app = express();
-var $ = require('jquery');
-var i18n=require('i18n-express');
-var fs = require('fs');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const i18n = require('i18n-express');
+const fs = require('fs');
 
 // set the view engine to ejs
-app.set('views',__dirname + '/views');
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
 app.use(expressLayouts);
 app.use(i18n({
   translationsPath: path.join(__dirname, 'assets/i18n'), // <--- use here. Specify translations files path.
   siteLangs: ['en'],
-  textsVarName: 'translation'
+  textsVarName: 'translation',
 }));
-app.use(express.static(__dirname + '/dist'));
-app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use(express.static(`${__dirname}/dist`));
+app.use('/bootstrap', express.static(`${__dirname}/node_modules/bootstrap/dist/`));
+app.use('/jquery', express.static(`${__dirname}/node_modules/jquery/dist/`));
 
 // invest index
-app.get('/', function(req, res) {
-  fs.readFile('./data/carousel.json', 'utf8', function (err, data) {
+app.get('/', (req, res) => {
+  fs.readFile('./data/carousel.json', 'utf8', (err, data) => {
     if (err) throw err;
-    var myVar = JSON.parse(data);
-    res.render('pages/home', {locals: myVar});
+    const myVar = JSON.parse(data);
+    res.render('pages/home', { locals: myVar });
   });
 });
 
-app.get('/articles/:id', function(req, res) {
-  fs.readFile('./data/carousel.json', 'utf8', function (err, data) {
+app.get('/articles/:id', (req, res) => {
+  fs.readFile('./data/carousel.json', 'utf8', (err, data) => {
     if (err) throw err;
-    var myVar = JSON.parse(data);
-    res.render('pages/article-details', {locals: myVar[req.params.id-1]});
+    const myVar = JSON.parse(data);
+    res.render('pages/article-details', { locals: myVar[req.params.id - 1] });
   });
 });
-
-if (!module.parent) {
-  // listen on port config.port
-  app.listen(1221, onStarted);
-  app.on('error', onError);
-  app.on('listening', onListening);
-}
 
 function onStarted() {
-  console.info(`Server started on port 1221`);
+  console.info('Server started on port 1221');
 }
 
 function onError(e) {
@@ -54,6 +45,14 @@ function onError(e) {
 }
 
 function onListening() {
-  console.info(`Server is listening on port 1221`);
+  console.info('Server is listening on port 1221');
 }
+if (!module.parent) {
+  // listen on port config.port
+  app.listen(1221, onStarted);
+  app.on('error', onError);
+  app.on('listening', onListening);
+}
+
+
 module.exports = app;
